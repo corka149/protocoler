012bca7707713cf60dd7db53561f32ca14fd6f57
corka149 <corka149@mailbox.org>
2021-10-04 22:18:04 +0200

    Implemented creating and deleting entries

--- cmd/protocoler.go
+++ cmd/protocoler.go
@@ -16,7 +16,7 @@ func Execute() {
 		Short: "A minimalistic typer for protocols",
 		Long: `A Fast and minimalistic protocol generator built powered by Cobra & Go.
                 It can output the protocol in different formats.`,
-		RunE: protocol,
+		RunE: protocolCmd,
 	}
 
 	if err := rootCmd.Execute(); err != nil {
@@ -27,11 +27,20 @@ func Execute() {
 
 // ===== PRIVATE =====
 
+type lineReaderFn func() (string, error)
+
 type protocolEntry struct {
 	entryType string
 	saidBy    string
 	text      string
 	timestamp time.Time
+}
+
+func (pe protocolEntry) String() string {
+	eType := strings.ToUpper(pe.entryType)
+	timestamp := pe.timestamp.Format("2006-01-02 15:04")
+
+	return fmt.Sprintf("%s %s - %s: %s", timestamp, eType, pe.saidBy, pe.text)
 }
 
 const (
@@ -52,63 +61,145 @@ func printUsage() {
 	fmt.Printf("\nEnter: (i) add Info, (d) add Decision, (t) add Task, (r) Remove entry, (entryId) edit entry OR (q) for Quit: ")
 }
 
-func protocol(cmd *cobra.Command, args []string) error {
+func protocolCmd(cmd *cobra.Command, args []string) error {
 	reader := bufio.NewReader(os.Stdin)
-	printUsage()
-	input, err := reader.ReadString('\n')
-	protocolEntries := make([]protocolEntry, 10)
 
-	input = strings.TrimSpace(input)
+	var lineReader = func() (string, error) {
+		input, err := reader.ReadString('\n')
 
-	for input != quitMarker && err == nil {
+		if err != nil {
+			return "", err
+		}
 
-		err = handleNextCmd(input, protocolEntries)
+		return strings.TrimSpace(input), nil
+	}
 
-		if err == nil {
-			printUsage()
-			input, err = reader.ReadString('\n')
-			input = strings.TrimSpace(input)
+	protocolEntries := make([]*protocolEntry, 0)
+
+	protocolEntries, err := protocol(lineReader, protocolEntries)
+
+	if err != nil {
+		return err
+	}
+
+	// New
+	fmt.Println("===== ===== ===== ===== =====")
+
+	for _, entry := range protocolEntries {
+		if entry != nil {
+			fmt.Println(entry)
 		}
 	}
 
-	return err
+	return nil
 }
 
-func handleNextCmd(input string, protocolEntries []protocolEntry) error {
+func protocol(lineReader lineReaderFn, protocolEntries []*protocolEntry) ([]*protocolEntry, error) {
+	printUsage()
+	input, err := lineReader()
+
+	for input != quitMarker && err == nil {
+
+		protocolEntries, err = handleNextInput(lineReader, input, protocolEntries)
+
+		if err == nil {
+			printUsage()
+			input, err = lineReader()
+		}
+	}
+
+	return protocolEntries, err
+}
+
+func handleNextInput(lineReader lineReaderFn, input string, protocolEntries []*protocolEntry) ([]*protocolEntry, error) {
+	var err error
+
+	entryType, isType := protocolEntryType[input]
+
 	// Append new entry
-	markerType, isType := protocolEntryType[input]
 	if isType {
-		if err := createEntry(protocolEntries, markerType); err != nil {
-			return err
+
+		protocolEntries, err = createEntry(lineReader, protocolEntries, entryType)
+
+		if err != nil {
+			return nil, err
 		}
 	}
 
 	// Remove entry
 	if input == removeMarker {
-		removeEntry(protocolEntries)
+		protocolEntries, err = removeEntry(lineReader, protocolEntries)
 	}
 
 	// Edit entry
 	if input != removeMarker && !isType {
 		fmt.Println(input)
+
 		possibleIndex, err := strconv.Atoi(input)
-		fmt.Println(possibleIndex)
+
 		if err == nil && possibleIndex < len(protocolEntries) {
-			editEntry(protocolEntries, possibleIndex)
+			editEntry(lineReader, protocolEntries, possibleIndex)
 		}
 	}
-	return nil
+
+	return protocolEntries, nil
 }
 
-func createEntry(entries []protocolEntry, markerType string) error {
-	fmt.Printf("Create entry of type '%s'\n", markerType)
-	return nil
+func createEntry(lineReader lineReaderFn, protocolEntries []*protocolEntry, entryType string) ([]*protocolEntry, error) {
+	fmt.Printf("Create entry of type '%s'\n", entryType)
+
+	fmt.Println("---Said by:")
+	saidBy, err := lineReader()
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("---Note:")
+	note, err := lineReader()
+
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+
+	newEntry := &protocolEntry{
+		entryType: entryType,
+		saidBy:    saidBy,
+		text:      note,
+		timestamp: now,
+	}
+
+	protocolEntries = append(protocolEntries, newEntry)
+	fmt.Printf("\nAdded entry with ID %d\n", len(protocolEntries)-1)
+
+	return protocolEntries, nil
 }
 
-func removeEntry(protocolEntries []protocolEntry) {
-	fmt.Println("Delete an entry ...")
+func removeEntry(lineReader lineReaderFn, protocolEntries []*protocolEntry) ([]*protocolEntry, error) {
+	fmt.Println("Delete an entry by ID:")
+
+	indexStr, err := lineReader()
+
+	if err != nil {
+		return protocolEntries, err
+	}
+
+	index, err := strconv.Atoi(indexStr)
+
+	if err != nil {
+		fmt.Println(err)
+		return protocolEntries, nil
+	}
+
+	// Set to nil is fast and cheap
+	protocolEntries[index] = nil
+
+	return protocolEntries, nil
 }
 
-func editEntry(protocolEntries []protocolEntry, index int) {
+func editEntry(lineReader lineReaderFn, protocolEntries []*protocolEntry, index int) {
+
 	fmt.Printf("Edit entry %d\n", index)
 }
