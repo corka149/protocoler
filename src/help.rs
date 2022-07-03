e5847174ca398ad0deca145dc20ad5423882de2b
corka149 <corka149@mailbox.org>
2022-07-03 13:17:38 +0200

    Extend help

--- src/help.rs
+++ src/help.rs
@@ -5,7 +5,9 @@ pub fn new() -> Dialog {
         .child("a", TextView::new("Add below"))
         .child("A", TextView::new("Add above"))
         .child("d", TextView::new("Delete an item"))
-        .child("e", TextView::new("Edit current item"));
+        .child("e", TextView::new("Edit current item"))
+        .child("-", TextView::new( "------------------"))
+        .child("q", TextView::new( "Quit"));
 
     Dialog::around(help)
         .button("Cancel", |s| {
