d1e95e64165c8c1e9ded664fcf69a5fce6800229
corka149 <corka149@mailbox.org>
2022-07-03 13:39:59 +0200

    Layout hint and table

--- src/help.rs
+++ src/help.rs
@@ -1,16 +1,26 @@
-use cursive::views::{Dialog, ListView, TextView};
+use cursive::traits::*;
+use cursive::views::{Dialog, LinearLayout, ListView, Panel, TextView};
 
-pub fn new() -> Dialog {
+pub fn help_menu() -> Dialog {
     let help = ListView::new()
         .child("a", TextView::new("Add below"))
         .child("A", TextView::new("Add above"))
         .child("d", TextView::new("Delete an item"))
         .child("e", TextView::new("Edit current item"))
-        .child("-", TextView::new( "------------------"))
-        .child("q", TextView::new( "Quit"));
+        .child("-", TextView::new("------------------"))
+        .child("q", TextView::new("Quit"));
 
     Dialog::around(help)
         .button("Cancel", |s| {
             s.pop_layer();
         })
 }
+
+pub fn hint_bar() -> LinearLayout {
+    LinearLayout::horizontal().child(
+        Panel::new(
+            TextView::new("x = menu")
+        ).title("Hint").full_width()
+    )
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ extern crate chrono;
 extern crate cursive;
 extern crate cursive_table_view;
 
+use cursive::traits::*;
+use cursive::views::{Dialog, LinearLayout, Panel, StackView, TextView};
+use cursive_table_view::TableView;
+
+use crate::table::{BasicColumn, EntryType, ProtocolEntry};
+
 mod style;
 mod table;
 mod help;
-
-use cursive::traits::*;
-use cursive::views::Dialog;
-
-use crate::table::{EntryType, ProtocolEntry};
-
 
 /// MAIN
 fn main() {
@@ -18,20 +18,35 @@ fn main() {
 
     let mut table = table::new();
 
-    table.insert_item(ProtocolEntry::new(
-        EntryType::Info, "Alice".to_string(), "Let`s go".to_string()
-    ));
+    dummy_data(&mut table);
+
+    let full_view = LinearLayout::vertical()
+        .child(table.full_height().with_name("main_table"))
+        .child(help::hint_bar())
+        .full_screen();
 
     siv.add_fullscreen_layer(
-        Dialog::around(table.with_name("main_table"))
+        Dialog::around(full_view)
             .title("Protocoler")
             .full_screen(),
     );
 
     siv.add_global_callback('q', |s| s.quit());
-    siv.add_global_callback('x', |s| s.add_layer(help::new()));
+    siv.add_global_callback('x', |s| s.add_layer(help::help_menu()));
 
     siv.update_theme(style::set_terminal_default);
 
     siv.run();
+}
+
+fn dummy_data(table: &mut TableView<ProtocolEntry, BasicColumn>) {
+    table.insert_item(ProtocolEntry::new(
+        EntryType::Info, "Alice".to_string(), "Let`s go".to_string(),
+    ));
+    table.insert_item(ProtocolEntry::new(
+        EntryType::Decision, "Bob".to_string(), "Are we done?".to_string(),
+    ));
+    table.insert_item(ProtocolEntry::new(
+        EntryType::Task, "Ceasar".to_string(), "yep yep yep".to_string(),
+    ));
 }
